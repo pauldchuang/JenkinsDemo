31f120647c677fc03b46c0829d4649175c2f703a
Paul Chuang <paul_chuang@trendmicro.com>
2021-08-25 10:10:56 +0800

    commit

--- file1.sh
+++ file1.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-ARG="$1"
-echo "$ARG"
+ARG=$'# line 1"
+# line 2"
+# line 3"'
+sh file1.sh "$ARG"
--- file2.sh
+++ file2.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-ARG=$'# line 1"
-# line 2"
-# line 3"'
-sh file1.sh "$ARG"
+ARG="$1"
+echo "$ARG"
